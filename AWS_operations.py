ddc16752533916c5ed822cf4ca93ea8d9c56ab54
SrVitinho <55586232+SrVitinho@users.noreply.github.com>
2023-09-12 20:58:13 -0300

    +AWS migration P2 +Input bucket integration +Stop Ec2 function +General fixes and rename

---
+++ AWS_operations.py
@@ -0,0 +1,48 @@
+import boto3
+import botocore
+from keys_aws import access_key
+from keys_aws import private_key
+
+session = boto3.Session(aws_access_key_id=access_key, aws_secret_access_key=private_key)
+region = 'sa-east-1'
+
+
+def get_wordx():
+
+    BUCKET = 'inputbucketslide'
+
+    s3 = session.resource('s3')
+
+    input_bucket = s3.Bucket(BUCKET)
+
+    for my_bucket_object in input_bucket.objects.all():
+        KEY = my_bucket_object.key
+    try:
+        save_path = '/home/ec2-user/input_file/' + KEY
+        input_bucket.download_file(Key=KEY, Filename=save_path)
+    except Exception as err:
+        print(err)
+        # add notification off error
+
+
+def clean_bucket():
+    BUCKET = 'inputbucketslide'
+
+    input_bucket = session.client('s3')
+
+    for my_bucket_object in input_bucket.objects.all():
+        KEY = my_bucket_object.key
+        input_bucket.delete_object(Bucket=BUCKET, Key=KEY)
+
+
+def kill_ec2():
+    client = session.client('ec2')
+    try:
+        client.stop_instances(
+            InstanceIds=[
+                'i-07151776ee1c83765',
+            ],
+            DryRun=False
+        )
+    except Exception as err:
+        print(err)
--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from PowerPoint import Ppt
 from Text_Manipulation import get_keywords
 from Input import pdf_text
 from llama_querys import query_llama
-import input_bucket
+import AWS_operations
 
 if __name__ == '__main__':
-    input_bucket.get_wordx()
+    AWS_operations.get_wordx()
     word_text_raw = Input.pdf_text()
     word_text = Text_Manipulation.clean_text(word_text_raw)
     keywords = Text_Manipulation.clean_key_phrases(word_text)
-    print('--------------------->' + keywords.__str__())
     resumos = query_llama(keywords)
     prs = Ppt('teste1', resumos)
     for item in range(keywords):
         prs.add_page(keywords[item][0])
     prs.save()
-
+    AWS_operations.clean_bucket()
+    AWS_operations.kill_ec2()
